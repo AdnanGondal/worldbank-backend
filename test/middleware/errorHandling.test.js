14f31d96aef0132e36884b5bc8a465d49af2fde3
Adnan Gondal <Adnan.Gondal@ukc02h726uq6lt.cust.communityfibre.co.uk>
2023-08-11 17:15:46 +0100

    tests updated

--- test/middleware/errorHandling.test.js
+++ test/middleware/errorHandling.test.js
@@ -1,84 +1,88 @@
-const sinon = require('sinon');
-const chai = require('chai');
+const sinon = require("sinon");
+const chai = require("chai");
 const {
-  errorLogger,
-  errorHandler,
-  invalidPathHandler,
-} = require('../../src/middleware/errorHandling');
+	errorLogger,
+	errorHandler,
+	invalidPathHandler,
+} = require("../../src/middleware/errorHandling");
 
 const { expect } = chai;
 
-describe('errorHandling middleware', () => {
-  describe('errorLogger middleware', () => {
-    let consoleStub;
-    let nextStub;
+describe("errorHandling middleware", () => {
+	describe("errorLogger middleware", () => {
+		let consoleStub;
+		let nextStub;
 
-    beforeEach(() => {
-      consoleStub = sinon.stub(console, 'log');
-      nextStub = sinon.stub();
-    });
+		beforeEach(() => {
+			consoleStub = sinon.stub(console, "log");
+			nextStub = sinon.stub();
+		});
 
-    afterEach(() => {
-      consoleStub.restore();
-    });
+		afterEach(() => {
+			consoleStub.restore();
+		});
 
-    it('should log the error message and call next', () => {
-      const error = new Error('test error');
-      errorLogger(error, null, null, nextStub);
-      expect(consoleStub.calledOnceWith(`error ${error.message}`)).to.be.true;
-      expect(nextStub.calledOnce).to.be.true;
-    });
-  });
+		it("should log the error message and call next", () => {
+			const error = new Error("test error");
+			errorLogger(error, null, null, nextStub);
+			expect(consoleStub.calledOnceWith(`error ${error.message}`)).to.be.true;
+			expect(nextStub.calledOnce).to.be.true;
+		});
+	});
 
-  describe('errorHandler middleware', () => {
-    it('should set the response status and send error message for general error', () => {
-      const error = new Error('test error');
-      const status = 400;
-      const response = {
-        header: sinon.stub(),
-        status: sinon.stub().returnsThis(),
-        send: sinon.stub(),
-      };
-      const nextStub = sinon.stub();
+	describe("errorHandler middleware", () => {
+		it("should set the response status and send error message for general error", () => {
+			const error = new Error("test error");
+			const status = 500;
+			const response = {
+				header: sinon.stub(),
+				status: sinon.stub().returnsThis(),
+				send: sinon.stub(),
+			};
+			const nextStub = sinon.stub();
 
-      errorHandler(error, null, response, nextStub);
+			errorHandler(error, null, response, nextStub);
 
-      expect(response.header.calledOnceWith('Content-Type', 'application/json')).to.be.true;
-      expect(response.status.calledOnceWith(status)).to.be.true;
-      expect(response.send.calledOnceWith(error.message)).to.be.true;
-    });
-  });
+			expect(response.header.calledOnceWith("Content-Type", "application/json"))
+				.to.be.true;
+			expect(response.status.calledOnceWith(status)).to.be.true;
+			expect(response.send.calledOnceWith({ message: error.message })).to.be
+				.true;
+		});
+	});
 
-  it('should set the response status and send error message for InvalidRequest', () => {
-    const error = new Error('test error');
-    const status = 400;
-    const response = {
-      header: sinon.stub(),
-      status: sinon.stub().returnsThis(),
-      send: sinon.stub(),
-    };
-    const nextStub = sinon.stub();
+	it("should set the response status and send error message for InvalidRequest", () => {
+		const error = new Error("test error");
+		const status = 500;
+		const response = {
+			header: sinon.stub(),
+			status: sinon.stub().returnsThis(),
+			send: sinon.stub(),
+		};
+		const nextStub = sinon.stub();
 
-    errorHandler(error, null, response, nextStub);
+		errorHandler(error, null, response, nextStub);
 
-    expect(response.header.calledOnceWith('Content-Type', 'application/json')).to.be.true;
-    expect(response.status.calledOnceWith(status)).to.be.true;
-    expect(response.send.calledOnceWith(error.message)).to.be.true;
-  });
+		expect(response.header.calledOnceWith("Content-Type", "application/json"))
+			.to.be.true;
+		expect(response.status.calledOnceWith(status)).to.be.true;
+		expect(response.send.calledOnceWith({ message: error.message })).to.be.true;
+	});
 
-  describe('invalidPathHandler middleware', () => {
-    it('should set the response status and send error message', () => {
-      const request = {};
-      const response = {
-        status: sinon.stub().returnsThis(),
-        send: sinon.stub(),
-      };
-      const nextStub = sinon.stub();
+	describe("invalidPathHandler middleware", () => {
+		it("should set the response status and send error message", () => {
+			const request = {};
+			const response = {
+				status: sinon.stub().returnsThis(),
+				send: sinon.stub(),
+			};
+			const nextStub = sinon.stub();
 
-      invalidPathHandler(request, response, nextStub);
+			invalidPathHandler(request, response, nextStub);
 
-      expect(response.status.calledOnceWith(400)).to.be.true;
-      expect(response.send.calledOnceWith('Invalid Path')).to.be.true;
-    });
-  });
+			expect(response.status.calledOnceWith(400)).to.be.true;
+			expect(response.send.calledOnceWith({ message: "Invalid Path" })).to.be
+				.true;
+		});
+	});
 });
